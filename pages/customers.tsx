f29ec0057897dffca1285cc6b70a8e8731136005
bono43 <bono43@nexondv.com>
2023-03-29 10:38:18 +0700

    complete admin bg

--- pages/customers.tsx
+++ pages/customers.tsx
@@ -22,7 +22,24 @@ const customers = () => {
               <li
                 key={idx}
                 className='bg-gray-50 hover:bg-gray-100 rounded-lg my-3 p-2 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer'
-              ></li>
+              >
+                <div className='flex items-center'>
+                  <div className='bg-purple-100 p-3 rounded-lg'>
+                    <BsPersonFill className='text-purple-800' />
+                  </div>
+                  <p className='pl-4'>
+                    {order.name.first + ' ' + order.name.last}
+                  </p>
+                </div>
+                <p className='text-gray-600 sm:text-left text-right'>
+                  {order.name.first}@gmail.com
+                </p>
+                <p className='hidden md:flex'>{order.date}</p>
+                <div className='sm:flex hidden justify-between items-center'>
+                  <p>{order.method}</p>
+                  <BsThreeDotsVertical />
+                </div>
+              </li>
             ))}
           </ul>
         </div>
