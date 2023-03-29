8dd7dc174d8092a688b101da8d5eff2032047c8f
bono43 <bono43@nexondv.com>
2023-03-29 17:38:38 +0700

    add some new package

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import { Button } from '@mantine/core';
+import { IconDatabase } from '@tabler/icons-react';
+import React from 'react';
 
 const Header = () => {
   return (
     <div className='flex justify-between px-4 pt-4'>
       <h2>Dashboard</h2>
       <h2>Welcome Back, Bono</h2>
+      <Button className='bg-white' leftIcon={<IconDatabase />} variant='white'>
+        Connect to database
+      </Button>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
 import Sidebar from '@/components/Sidebar';
 import '@/styles/globals.css';
+import { MantineProvider } from '@mantine/core';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Sidebar {...pageProps}>
-      <Component {...pageProps} />
-    </Sidebar>
+    <>
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        <Sidebar {...pageProps}>
+          <Component {...pageProps} />
+        </Sidebar>
+      </MantineProvider>
+    </>
   );
 }
