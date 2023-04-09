0453ceb3b8d2f11003280e7da6659b117d9fcb80
ngocxxu <ngocquach43@gmail.com>
2023-04-09 23:43:46 +0700

    config redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
 import Sidebar from '@/components/Sidebar/Sidebar';
+import { wrapper } from '@/store/store';
 import '@/styles/globals.css';
 import { MantineProvider } from '@mantine/core';
 import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <MantineProvider withGlobalStyles withNormalizeCSS>
@@ -14,3 +15,5 @@ export default function App({ Component, pageProps }: AppProps) {
     </>
   );
 }
+
+export default wrapper.withRedux(App);
--- pages/users/user-list.tsx
+++ pages/users/user-list.tsx
@@ -1,8 +1,13 @@
 import { TextHeader } from '@/components/Text';
+import { selectAuthState, setAuthState } from '@/store/reducer/authSlice';
 import { USER_LIST_ROUTE } from '@/utils/consts';
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 const UserList = () => {
+  const authState = useSelector(selectAuthState);
+  const dispatch = useDispatch();
+
   return (
     <div>
       <TextHeader
@@ -10,6 +15,18 @@ const UserList = () => {
         titleButton='Add User'
         path={USER_LIST_ROUTE}
       />
+      <div>
+        <div>{authState ? 'Logged in' : 'Not Logged In'}</div>
+        <button
+          onClick={() =>
+            authState
+              ? dispatch(setAuthState(false))
+              : dispatch(setAuthState(true))
+          }
+        >
+          {authState ? 'Logout' : 'LogIn'}
+        </button>
+      </div>
     </div>
   );
 };
